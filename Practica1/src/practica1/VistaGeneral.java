9f5b3ec803d5d22fbd4238c1088edb9478d572e5
liobouchan <leo.bouchan@gmail.com>
2015-04-24 17:30:07 -0500

    Empezando a pedir cadenas

--- Practica1/src/practica1/VistaGeneral.java
+++ Practica1/src/practica1/VistaGeneral.java
@@ -36,6 +36,11 @@ public class VistaGeneral extends javax.swing.JFrame {
         textElementoNuevo = new javax.swing.JTextField();
         botonAgregarElementoalAbecedario = new javax.swing.JButton();
         labelAbecedarioActual = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        textCadena1 = new javax.swing.JTextField();
+        textCadena2 = new javax.swing.JTextField();
+        botonLongitud = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -50,6 +55,12 @@ public class VistaGeneral extends javax.swing.JFrame {
 
         labelAbecedarioActual.setText(".");
 
+        jLabel2.setText("Ingresar Cadena1 :");
+
+        jLabel3.setText("Ingresar Cadena2 :");
+
+        botonLongitud.setText("Longuitud");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -59,11 +70,22 @@ public class VistaGeneral extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(labelAbecedarioActual, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
+                        .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(textElementoNuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(textCadena1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(botonAgregarElementoalAbecedario)
+                        .addComponent(textCadena2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(textElementoNuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(botonAgregarElementoalAbecedario))
+                            .addComponent(botonLongitud))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -78,7 +100,17 @@ public class VistaGeneral extends javax.swing.JFrame {
                         .addComponent(botonAgregarElementoalAbecedario, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(labelAbecedarioActual)
-                .addContainerGap(262, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(textCadena1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(textCadena2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(botonLongitud)
+                .addContainerGap(110, Short.MAX_VALUE))
         );
 
         pack();
@@ -87,18 +119,23 @@ public class VistaGeneral extends javax.swing.JFrame {
     private void botonAgregarElementoalAbecedarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonAgregarElementoalAbecedarioActionPerformed
         // TODO add your handling code here:
         
+        String contenidoDelArreglo = "";
+        
+        //Agregar elementos al Arreglo
         caracter = String.valueOf(textElementoNuevo.getText());
         abecedario[numeroDeElemento] = caracter;
         numeroDeElemento ++;
         textElementoNuevo.setText("");
-        String value = "";
+        
+        //Imprimir los elementos actuales del arreglo en una Label
         for(int i=0 ; i<=numeroDeElemento; i++){
             if(abecedario[i] != null){
-                value += abecedario[i] + ",";
-                labelAbecedarioActual.setText(value);
+                contenidoDelArreglo += abecedario[i] + ",";
+                labelAbecedarioActual.setText(contenidoDelArreglo);
                 System.out.println(abecedario[i]);
             }
         }
+        
     }//GEN-LAST:event_botonAgregarElementoalAbecedarioActionPerformed
 
     /**
@@ -138,8 +175,13 @@ public class VistaGeneral extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botonAgregarElementoalAbecedario;
+    private javax.swing.JButton botonLongitud;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel labelAbecedarioActual;
+    private javax.swing.JTextField textCadena1;
+    private javax.swing.JTextField textCadena2;
     private javax.swing.JTextField textElementoNuevo;
     // End of variables declaration//GEN-END:variables
 }
